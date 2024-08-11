8e9b5e493597212fc736bd48526f850995248243
sakabiruddin <145967210+sakabiruddin@users.noreply.github.com>
2024-08-11 13:06:45 +0600

    initial commit

---
+++ src/app/dummy-users.ts
@@ -0,0 +1,37 @@
+export const DUMMY_USERS = [
+  {
+    id: 'u1',
+    name: 'Tasmia',
+    avatar: 'user-1.jpg',
+  },
+  {
+    id: 'u2',
+    name: 'Amrita',
+    avatar: 'user-2.jpg',
+  },
+  {
+    id: 'u3',
+    name: 'Shoptorshi',
+    avatar: 'user-3.jpg',
+  },
+  {
+    id: 'u4',
+    name: 'Prottoy',
+    avatar: 'user-4.jpg',
+  },
+  {
+    id: 'u5',
+    name: 'Saad',
+    avatar: 'user-5.jpg',
+  },
+  {
+    id: 'u6',
+    name: 'Ahona',
+    avatar: 'user-6.jpg',
+  },
+  {
+    id: 'u7',
+    name: 'Kashfia',
+    avatar: 'user-7.jpg',
+  },
+];
